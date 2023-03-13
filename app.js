3242a0b333967ec1d49c0ec1e78aaaed69554a7e
FIRDOUS AHMAD WANI <82380855+Firdous708@users.noreply.github.com>
2023-03-14 01:03:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let btnDownload = document.querySelector('#btn');
+let img = document.querySelector('#img');
 function generateQR(){
   document.querySelector("#qr-image").style.display = "block";
   btnDownload.style.display = "block";
@@ -12,11 +14,10 @@ function generateQR(){
     document.querySelector("#img").src = "https://api.qrserver.com/v1/create-qr-code/?size=240×240&data=" + QRtext;
   }
 }
-let btnDownload = document.querySelector('#btn');
-let img = document.querySelector('#img');
 
 
-// Must use FileSaver.js 2.0.2 because 2.0.3 has issues.
+
+
 btnDownload.addEventListener('click', () => {
     let imagePath = img.getAttribute('src');
     let fileName = getFileName(imagePath);
