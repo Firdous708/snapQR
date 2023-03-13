e17ca401644f37fda229c9fe46f1ec3ed0b0d680
FIRDOUS AHMAD WANI <82380855+Firdous708@users.noreply.github.com>
2023-03-14 00:52:16 +0530

    Create
    added functionality using js

---
+++ app.js
@@ -0,0 +1,28 @@
+function generateQR(){
+  document.querySelector("#qr-image").style.display = "block";
+  btnDownload.style.display = "block";
+  let QRtext = document.querySelector("#text").value;
+  if(QRtext.trim().length == 0){
+    document.querySelector("#qr-image .error").innerHTML = "Please enter text";
+  document.querySelector("#img").style.display = "none";
+    btnDownload.style.display = "none";
+  } else {
+    document.querySelector("#img").style.display = "block";
+    document.querySelector("#qr-image .error").innerHTML = "";
+    document.querySelector("#img").src = "https://api.qrserver.com/v1/create-qr-code/?size=240×240&data=" + QRtext;
+  }
+}
+let btnDownload = document.querySelector('#btn');
+let img = document.querySelector('#img');
+
+
+// Must use FileSaver.js 2.0.2 because 2.0.3 has issues.
+btnDownload.addEventListener('click', () => {
+    let imagePath = img.getAttribute('src');
+    let fileName = getFileName(imagePath);
+    saveAs(imagePath, fileName);
+});
+
+function getFileName(str) {
+    return str.substring(str.lastIndexOf('/') + 1)
+}
